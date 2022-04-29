5f04e9fa17c942b425dd2f9067aa50e61438ea35
P1ratRuleZZZ <p1ratrulezzz@users.noreply.github.com>
2022-04-29 15:17:44 +0300

    Text

--- src/app/items.go
+++ src/app/items.go
@@ -8,9 +8,13 @@ import (
 
 func item_show_info() {
 	info := `
-This tool is written bla bla bla
-To blabla bla visit this blabla bla bla
-Bla bla
+This tool is provided specially for Typical Society forum and shipped AS IS.
+This tool is open source and can be used by anyone. 
+Please note, this tool is only for demonstrating and non commercial use, 
+if you love some software - buy it and use freely.
+
+Visit: https://forum.losper.net/
+
 `
 	print(info)
 }
