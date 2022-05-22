4db5e7ca0f56048061e478ed4a747e587954874c
P1ratRuleZZZ <p1ratrulezzz@users.noreply.github.com>
2022-05-22 16:54:10 +0300

    GUI autopatch

--- src/app/gui.go
+++ src/app/gui.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -11,7 +12,9 @@ import (
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"jetbrainser/src/patchers"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -37,6 +40,10 @@ func gui() {
 
 	var appdataDirs []string
 	var files []string
+	var products []patchers.ProductInfo
+	var guiPrepareExit func()
+
+	files = []string{"test"}
 	bndData := files
 	bndFiles := binding.BindStringList(&bndData)
 
@@ -53,25 +60,36 @@ func gui() {
 
 		})
 
-	wdgLabelTop := widget.NewLabel("Select source *.vmoptions file from the list")
+	bndWidgetText := binding.NewString()
+	wdgListText := widget.NewLabelWithData(bndWidgetText)
+	wdgList.Hide()
+
+	wdgLabelTop := widget.NewLabel("Searching for products...")
 
 	step := 0
 	selectedIndex := 0
 	var ptrWdgButtonNext *widget.Button
 	var selectedSource, selectedAppdata, selectedKey int
-	wdgButtonNext := widget.NewButton("Next", func() {
+	wdgButtonNext := widget.NewButton("Patch All", func() {
 		wdgList.UnselectAll()
 
 		switch step {
-		// Source file selected
+		// Automatic patch mode
 		case 0:
+			step = 100
+			messages := doAutoPatch(tool, products)
+			wdgListText.SetText(strings.Join(messages, "\n"))
+			guiPrepareExit()
+			break
+		// Source file selected
+		case 1:
 			ptrWdgButtonNext.Disable()
 			bndData = appdataDirs
 			bndFiles.Reload()
 			selectedSource = selectedIndex
 			wdgLabelTop.SetText("Will patch " + files[selectedSource] + "\nSelect destination directory")
 			break
-		case 1:
+		case 2:
 			selectedAppdata = selectedIndex
 			wdgLabelTop.SetText("Will use " + appdataDirs[selectedAppdata] + "\nSelect key")
 			bndData = KeyListNameIndexed
@@ -79,10 +97,10 @@ func gui() {
 			ptrWdgButtonNext.SetText("Patch")
 
 			break
-		case 2:
+		case 3:
 			selectedKey = selectedIndex
 			wdgLabelTop.SetText("Patched with " + KeyListSlugIndexed[selectedKey])
-			ptrWdgButtonNext.SetText("Exit")
+			guiPrepareExit()
 			appdata := appdataDirs[selectedAppdata]
 			if selectedAppdata == 0 {
 				appdata = ""
@@ -94,6 +112,7 @@ func gui() {
 			} else {
 				wdgLabelTop.SetText("Patched!")
 			}
+			step = 100
 			break
 		default:
 			wndMain.Close()
@@ -103,7 +122,84 @@ func gui() {
 	})
 
 	ptrWdgButtonNext = wdgButtonNext
+	var wdgButtonRescanPtr *widget.Button
+	var wdgButtonManualPtr *widget.Button
+	wdgButtonManual := widget.NewButton("Switch to manual mode", func() {
+		wdgButtonRescanPtr.Hide()
+		wdgList.Show()
+		wdgListText.Hide()
+		wdgButtonManualPtr.Hide()
+		wdgButtonNext.Disable()
 
+		wdgProgressBar.Start()
+		wdgProgressBar.Show()
+
+		wdgButtonNext.SetText("Next")
+		wdgLabelTop.SetText("Searching for *.vmoptions files")
+
+		step = 1
+		filesChan := make(chan []string)
+		appdataDirsChan := make(chan []string)
+		go guiFindDirectories(tool, filesChan, appdataDirsChan)
+		stopTimer := make(chan bool, 1)
+		go guiMessagesProgress(wdgLabelTop, stopTimer)
+
+		files = <-filesChan
+		appdataDirs = <-appdataDirsChan
+		stopTimer <- true
+
+		bndData = files
+		bndFiles.Reload()
+		wdgProgressBar.Stop()
+		wdgProgressBar.Hide()
+		wdgLabelTop.SetText(fmt.Sprintf("Found %d files", len(files)))
+	})
+
+	wdgButtonManualPtr = wdgButtonManual
+
+	wdgButtonRescan := widget.NewButton("Rescan", func() {
+		bndData = []string{}
+		bndFiles.Reload()
+
+		productsChan := make(chan []patchers.ProductInfo)
+		go guiFindRunningProducts(tool, productsChan)
+
+		products = <-productsChan
+		close(productsChan)
+
+		if len(products) == 0 {
+			wdgLabelTop.SetText("No products were found")
+			bndWidgetText.Set("No running products were found. \n Make sure you have running products and press \"Rescan\" or use manual mode")
+			wdgButtonManual.Enable()
+			return
+		}
+
+		var productList []string
+		for _, productInfo := range products {
+			listItem := productInfo.ProductName + "(" + productInfo.VmoptionsSourcePath + ")"
+			productList = append(productList, listItem)
+		}
+
+		bndWidgetText.Set(strings.Join(productList, "\n"))
+
+		wdgProgressBar.Stop()
+		wdgProgressBar.Hide()
+		wdgButtonManual.Enable()
+		wdgButtonNext.Enable()
+		wdgLabelTop.SetText(fmt.Sprintf("Found %d running products", len(productList)))
+	})
+
+	guiPrepareExit = func() {
+		step = 100
+		wdgButtonNext.SetText("Exit")
+		wdgButtonNext.Enable()
+		wdgButtonManual.Disable()
+		wdgButtonRescan.Disable()
+	}
+
+	wdgButtonRescanPtr = wdgButtonRescan
+
+	wdgButtonManual.Disable()
 	wdgButtonNext.Disable()
 
 	wdgList.OnSelected = func(id widget.ListItemID) {
@@ -111,34 +207,62 @@ func gui() {
 		selectedIndex = id
 	}
 
-	go func() {
-		files = tool.FindVmoptionsFiles()
-
-		appdataDirs = tool.FindConfigDirectories()
-		appdataDirs = append([]string{"Patch in place"}, appdataDirs...)
-
-		bndData = files
-		bndFiles.Reload()
-
-		wdgProgressBar.Stop()
-		wdgProgressBar.Hide()
-
-		if len(files) == 0 {
-			wdgLabelTop.SetText("No *.vmoptions files were found")
-			step = -1
-			wdgButtonNext.Enable()
-			wdgButtonNext.SetText("Exit")
-		}
-	}()
+	wdgButtonRescan.OnTapped()
 
 	top := container.NewVBox(wdgLabelTop, wdgProgressBar)
+	buttons := container.NewAdaptiveGrid(3, wdgButtonRescan, wdgButtonManual, wdgButtonNext)
+	list := container.New(layout.NewMaxLayout(), wdgList, wdgListText)
 	content := container.New(
-		layout.NewBorderLayout(top, wdgButtonNext, nil, nil),
+		layout.NewBorderLayout(top, buttons, nil, nil),
 		top,
-		wdgButtonNext,
-		container.New(layout.NewMaxLayout(), wdgList),
+		buttons,
+		list,
 	)
 
 	wndMain.SetContent(content)
 	wndMain.ShowAndRun()
+}
+
+func guiFindDirectories(tool patchers.PatcherTool, files chan []string, appdataDirs chan []string) {
+	files <- tool.FindVmoptionsFiles()
+	close(files)
+
+	appdataDirsTmp := tool.FindConfigDirectories()
+	appdataDirsTmp = append([]string{"Patch in place"}, appdataDirsTmp...)
+
+	appdataDirs <- appdataDirsTmp
+	close(appdataDirs)
+}
+
+func guiFindRunningProducts(tool patchers.PatcherTool, products chan []patchers.ProductInfo) {
+	products <- tool.FindVmoptionsFromProcesses()
+}
+
+var messages []string = []string{
+	"Waiting...",
+	"Stil waiting...",
+	"This is taking soooo long....",
+	"Probably you should use an SSD?",
+	"Wow your computer is a potato...",
+	"Seriously... still running???",
+}
+
+func guiMessagesProgress(label *widget.Label, stopTimer chan bool) {
+	currentId := 0
+	waitDuration, _ := time.ParseDuration("5s")
+	stopTimer <- false
+	for !<-stopTimer {
+		if currentId < len(messages) {
+			label.SetText(messages[currentId])
+			currentId++
+		}
+
+		time.Sleep(waitDuration)
+		duration, _ := time.ParseDuration(fmt.Sprintf("%ds", rand.Intn(9)+1))
+		waitDuration += duration
+		stopTimer <- false
+	}
+
+	label.SetText("")
+	close(stopTimer)
 }
